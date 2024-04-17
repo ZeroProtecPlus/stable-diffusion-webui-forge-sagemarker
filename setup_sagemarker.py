d6a60342e7c90f42ae43a66a6a81decd1873bcb1
Juan Sebastian Giraldo <juangiraldo.sc@academia.umb.edu.co>
2024-04-17 12:42:46 -0500

    Added downloading extensions within the setup file

--- setup_sagemarker.py
+++ setup_sagemarker.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 from contextlib import suppress
 
-os.chdir("/home/studio-lab-user")
+os.chdir("/home/studio-lab-user")  
 
 def run_command(command, message):
     print(f"Please wait, {message} is loading...")
@@ -19,5 +19,28 @@ commands = [
     ('conda install -q -y glib=2.51.0', 'glib')
 ]
 
+def download_ext(command):
+    print("Downloading Extensions")
+    try:
+        subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("Extension downloaded successfully!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: Extension download failed with the following output:\n{e.stderr.decode('utf-8')}")
+
+extensions = [
+    'git clone https://github.com/NoCrypt/sd-fast-pnginfo.git',
+    'git clone https://github.com/etherealxx/batchlinks-webui.git',
+    'git clone https://github.com/Bing-su/adetailer.git',
+    'git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git',
+    'git clone https://github.com/BlafKing/sd-civitai-browser-plus.git',
+    'git clone https://github.com/zanllp/sd-webui-infinite-image-browsing.git',
+    'git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git'
+]
+
 for command, message in commands:
     run_command(command, message)
+
+os.chdir('~/stable-diffusion-webui-forge-sagemarker/extensions')  
+
+for extension in extensions:
+    download_ext(extension)
