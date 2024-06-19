a8bb4932d89766140706c76f338ab3afa0af24f2
Juan Sebastian Giraldo <juangiraldo.sc@academia.umb.edu.co>
2024-06-19 18:44:38 -0500

    PonyDiffusion XL model download added

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -156,9 +156,9 @@ def list_models():
     if shared.cmd_opts.no_download_sd_model or cmd_ckpt != shared.sd_model_file or os.path.exists(cmd_ckpt):
         model_url = None
     else:
-        model_url = "https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/realisticVisionV51_v51VAE.safetensors"
+        model_url = "https://civitai.com/api/download/models/290640?token=8c21c701b94e0d12cb2ce7f30e7e75cb"
 
-    model_list = modelloader.load_models(model_path=model_path, model_url=model_url, command_path=shared.cmd_opts.ckpt_dir, ext_filter=[".ckpt", ".safetensors"], download_name="realisticVisionV51_v51VAE.safetensors", ext_blacklist=[".vae.ckpt", ".vae.safetensors"])
+    model_list = modelloader.load_models(model_path=model_path, model_url=model_url, command_path=shared.cmd_opts.ckpt_dir, ext_filter=[".ckpt", ".safetensors"], download_name="ponyDiffusionV6XL_v6StartWithThisOne.safetensors", ext_blacklist=[".vae.ckpt", ".vae.safetensors"])
 
     if os.path.exists(cmd_ckpt):
         checkpoint_info = CheckpointInfo(cmd_ckpt)
--- setup_sagemarker.py
+++ setup_sagemarker.py
@@ -2,22 +2,8 @@ import os
 import subprocess
 from contextlib import suppress
 
-os.chdir("/home/studio-lab-user")  
+os.chdir("/workspace")  
 
-def run_command(command, message):
-    print(f"Please wait, {message} is loading...")
-    try:
-        subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"{message} completed successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {message} failed with the following output:\n{e.stderr.decode('utf-8')}")
-
-commands = [
-    ('conda install conda -y', 'Conda'),
-    ('conda update -n base conda -y', 'Update'),
-    ('conda install -n base python=3.10 -y', 'Python'),
-    ('conda install -q -y glib=2.51.0', 'glib')
-]
 
 def download_ext(command):
     print("Downloading Extensions")
@@ -28,19 +14,25 @@ def download_ext(command):
         print(f"Error: Extension download failed with the following output:\n{e.stderr.decode('utf-8')}")
 
 extensions = [
-    'git clone https://github.com/NoCrypt/sd-fast-pnginfo.git',
+    'git clone https://github.com/w-e-w/sd-fast-pnginfo.git',
     'git clone https://github.com/etherealxx/batchlinks-webui.git',
     'git clone https://github.com/Bing-su/adetailer.git',
     'git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git',
     'git clone https://github.com/BlafKing/sd-civitai-browser-plus.git',
     'git clone https://github.com/zanllp/sd-webui-infinite-image-browsing.git',
-    'git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git'
+    'git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git',
+    'git clone https://github.com/uwidev/sd_extension-prompt_formatter',
+    'git clone https://github.com/picobyte/stable-diffusion-webui-wd14-tagger',
+    'git clone https://github.com/mcmonkeyprojects/sd-dynamic-thresholding',
+    'git clone https://github.com/huchenlei/sd-webui-openpose-editor',
+    'git clone https://github.com/Mikubill/sd-webui-controlnet',
+    'git clone https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111',
+    'git clone https://github.com/hako-mikan/sd-webui-regional-prompter',
+    'git clone https://github.com/catppuccin/stable-diffusion-webui',
+    'git clone https://github.com/thomasasfk/sd-webui-aspect-ratio-helper'
 ]
 
-for command, message in commands:
-    run_command(command, message)
-
-os.chdir('/home/studio-lab-user/stable-diffusion-webui-forge-sagemarker/extensions')  
+os.chdir('workspace/stable-diffusion-webui-forge-sagemarker/extensions')  
 
 for extension in extensions:
     download_ext(extension)
